8d8fbc836adffd524099a4a4dbf650e5b5f2efb3
kamilksi <kamilksiazek002@gmail.com>
2022-08-11 17:37:24 +0200

    Starter project

---
+++ PokemonUI/PokemonUI/PokemonUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonUIApp.swift
+//  PokemonUI
+//
+//  Created by Kamil Książek on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokemonUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
