a9022aa03d1ac0551a29a4a40066d7ef503d5c0c
kamilksi <kamilksiazek002@gmail.com>
2022-08-11 18:14:45 +0200

    Create PokemonGridView

---
+++ PokemonUI/PokemonUI/Views/PokemonView/PokemonGridView.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonGridView.swift
+//  PokemonUI
+//
+//  Created by Kamil Książek on 11/08/2022.
+//
+
+import SwiftUI
+
+struct PokemonGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonGridView()
+    }
+}
