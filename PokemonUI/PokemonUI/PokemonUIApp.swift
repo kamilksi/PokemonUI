2a3d0df3cb4e8d273f83f784c0f6f689d04e7d19
kamilksi <kamilksiazek002@gmail.com>
2022-08-27 20:47:45 +0200

    Configure firebase

--- PokemonUI/PokemonUI/PokemonUIApp.swift
+++ PokemonUI/PokemonUI/PokemonUIApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct PokemonUIApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
